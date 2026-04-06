7ed21d6ee0cf0c4ea92fc5950b0a796eaed4015b
ibrahimaltr17 <ibrahimaltr17@gmail.com>
2026-04-06 14:52:12 +0600

    set JWT API

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleware 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 
